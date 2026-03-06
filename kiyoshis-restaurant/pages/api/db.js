d9485afe46ce0cb1cf847da4bf01db69bcdf84e3
Maxwell <maxwell.dinsmore@dcmail.ca>
2026-03-06 15:34:11 -0500

    added env.example

--- kiyoshis-restaurant/pages/api/db.js
+++ kiyoshis-restaurant/pages/api/db.js
@@ -1,6 +1,5 @@
 import postgres from 'postgres'
 
-console.log(process.env.DBNAME);
 const DATABASE_URL= `postgres://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBURL}:${process.env.DBPORT}/${process.env.DBNAME}`;
 const sql = postgres(DATABASE_URL, {
   max: 10, // connection pool size
